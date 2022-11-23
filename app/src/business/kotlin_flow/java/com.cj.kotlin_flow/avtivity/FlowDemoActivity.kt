a0f6ad4658b8a9236ae181e76f0d68b67ce75b96
472743062@qq.com <472743062@qq.com>
2022-11-23 15:18:44 +0800

    协程--flow稍复杂操作符

--- app/src/business/kotlin_flow/java/com.cj.kotlin_flow/avtivity/FlowDemoActivity.kt
+++ app/src/business/kotlin_flow/java/com.cj.kotlin_flow/avtivity/FlowDemoActivity.kt
@@ -46,6 +46,7 @@ class FlowDemoActivity : BaseBindingActivity<ActivityFlowDemoBinding>() {
         //5. 執行測試就 OK了
         @JvmStatic
         fun main(args: Array<String>) {
+            //简单操作符
             runBlocking {
                 val flow = flowOf(1, 2, 3, 4, 5, 6)
                 //map,filter等操作符要么连着flowOf写,要么之后连着collect,不然不生效
@@ -55,9 +56,54 @@ class FlowDemoActivity : BaseBindingActivity<ActivityFlowDemoBinding>() {
                     println(it)
                 }.map { //map是对发射的数据做运算
                     it * it
-                }.collect {
-                    println(it)
+                }.debounce(500L) //表示只有两条数据之间的间隔超过500毫秒才能发送成功。
+                    .collect {//collect数据,终端操作符,也可以用collectLatest收集最新的数据,前一个还未处理完的数据就会中端处理直接处理最新的一个
+                        println(it)
+                    }
+            }
+            //复杂一点的操作符
+            runBlocking {
+                val flow = flow<Int> {
+                    emit(1)
+                    delay(500)
+                    emit(2)
+                    emit(3)
+                    delay(600)
+                    emit(4)
                 }
+                flow
+                    .debounce(500L) //表示只有两条数据之间的间隔超过500毫秒才能发送成功。1和2之间隔了500,没有超过500,所以2发射失败,到发射3的时候,已经超过500,所以3可以发射成功,4是最后一个,所以4可以成功,但是我不理解为什么4是最后一个就可以发射成功
+                    .sample(1000L)//表示一秒钟之内只允许发送一个数据
+                    .collect {//collect数据,终端操作符,也可以用collectLatest收集最新的数据,前一个还未处理完的数据就会中端处理直接处理最新的一个
+                        println(it)
+                    }
+            }
+            //前面几个操作符都是最终需要collect函数来收集结果,下面的操作符不需要借助collect函数,自己就可以终结flow流程
+            runBlocking {
+                val flow = flow<Int> {
+                    for (i in 1..100) {
+                        emit(i)
+                    }
+                }
+                //reduce函数会通过参数给我们一个Flow的累积值和一个Flow的当前值，我们可以在函数体中对它们进行一定的运算，运算的结果会作为下一个累积值继续传递到reduce函数当中
+                val result = flow.reduce { accumulator, value ->
+                    //这里需要注意的是，reduce函数是一个终端操作符函数，它的后面不可以再接其他操作符函数了，而是只能获取最终的运行结果。
+                    accumulator + value
+                }
+                println(result)//结果为5050
+                //fold与reduce完全一样,不同的是fold需要传入初始值
+                val result2 = flow.fold(1) { acc, value ->
+                    acc + value
+                }
+                println(result2) //结果为5051,因为初始值是1
+                val result3 = flow {
+                    for (i in ('A'..'Z')) {
+                        emit(i.toString())
+                    }
+                }.fold("English:") { acc, value ->
+                    acc + value
+                }
+                println(result3) //通过fold可以实现字符串的拼接
             }
         }
     }
